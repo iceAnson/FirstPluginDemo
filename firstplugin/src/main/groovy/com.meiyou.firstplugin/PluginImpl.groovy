2ae852487550c4eb3c6514966b9a27f9f3ed6894
linwuhan <452825089@qq.com>
2017-04-28 15:25:42 +0800

    集成自定义Transform

--- firstplugin/src/main/groovy/com.meiyou.firstplugin/PluginImpl.groovy
+++ firstplugin/src/main/groovy/com.meiyou.firstplugin/PluginImpl.groovy
@@ -1,13 +1,153 @@
 package com.meiyou.firstplugin
 
+import com.android.build.api.transform.*
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.internal.pipeline.TransformManager
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.FileUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-public class PluginImpl implements Plugin<Project> {
+
+public class PluginImpl extends Transform implements Plugin<Project> {
     void apply(Project project) {
         project.task('testTask') << {
-            println "Hello gradle plugin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+             println "Hello gradle plugin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+         }
+
+         //task耗时监听
+         project.gradle.addListener(new TimeListener())
+
+        //遍历class文件和jar文件，在这里可以进行class文件asm文件替换
+        def android = project.extensions.getByType(AppExtension);
+        android.registerTransform(this)
+    }
+
+
+    @Override
+    public String getName() {
+        return "PluginImpl";
+    }
+
+    @Override
+    public Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS;
+    }
+
+    @Override
+    public Set<QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT;
+    }
+
+    @Override
+    public boolean isIncremental() {
+        return false;
+    }
+    /*@Override
+    void transform(Context context, Collection<TransformInput> inputs,
+                   Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider,
+                   boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        println '//===============asm visit start===============//'
+
+
+        def startTime = System.currentTimeMillis()
+
+        inputs.each { TransformInput input ->
+
+            input.directoryInputs.each {
+                DirectoryInput directoryInput ->
+
+                    if (directoryInput.file.isDirectory()) {
+                        directoryInput.file.eachFileRecurse { File file ->
+                            def name = file.name
+                            if (name.endsWith(".class") && !name.startsWith("R\$") &&
+                                    !"R.class".equals(name) && !"BuildConfig.class".equals(name)) {
+
+                                println name + ' is changing...'
+
+                                ClassReader cr = new ClassReader(file.bytes)
+                                ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
+                                ClassVisitor cv = new CostClassVisitor(cw)
+
+                                cr.accept(cv, EXPAND_FRAMES)
+
+                                byte[] code = cw.toByteArray()
+
+                                FileOutputStream fos = new FileOutputStream(
+                                        file.parentFile.absolutePath + File.separator + name)
+                                fos.write(code)
+                                fos.close()
+                            }
+                        }
+                    }
+
+                    def dest = outputProvider.getContentLocation(directoryInput.name,
+                            directoryInput.contentTypes, directoryInput.scopes,
+                            Format.DIRECTORY)
+
+
+                    FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+            input.jarInputs.each { JarInput jarInput ->
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+
+                FileUtils.copyFile(jarInput.file, dest)
+            }
         }
-        //task耗时监听
-        project.gradle.addListener(new TimeListener())
+
+        def cost = (System.currentTimeMillis() - startTime) / 1000
+
+        println "plugin cost $cost secs"
+        println '//===============asm visit end===============//'
+    }*/
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs,
+                   TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+
+        //遍历inputs里的TransformInput
+        inputs.each { TransformInput input ->
+            //遍历input里边的DirectoryInput
+            input.directoryInputs.each {
+                DirectoryInput directoryInput ->
+                    //是否是目录
+                    if (directoryInput.file.isDirectory()) {
+                        //遍历目录
+                        directoryInput.file.eachFileRecurse {
+                            File file ->
+                                //def filename = file.name;
+                                println '//PluginImpl find file:' + file.getAbsolutePath()
+                                //project.logger.
+                        }
+                    }
+                    //处理完输入文件之后，要把输出给下一个任务
+                    def dest = outputProvider.getContentLocation(directoryInput.name,
+                            directoryInput.contentTypes, directoryInput.scopes,
+                            Format.DIRECTORY)
+                    FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+
+            input.jarInputs.each { JarInput jarInput ->
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                println '//PluginImpl find Jar:' + jarInput.getFile().getAbsolutePath()
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+
     }
 }
